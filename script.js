3873313df9e2ccbecd33c74a0ef52222f8884421
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-01-23 13:30:44 -0300

    playRound and playGame functions were built, the program is finished

--- script.js
+++ script.js
@@ -5,23 +5,42 @@ function getComputerChoice (){
     let computerPlay;
     let random = Math.random();
     if (random <= 1/3){
-        computerPlay = "Rock";
+        computerPlay = "rock";
     } else if (random <= 2/3){
-        computerPlay = "Paper";
+        computerPlay = "paper";
     } else {
-        computerPlay = "Scissors";
+        computerPlay = "scissors";
     }
     return computerPlay;
 }
-
 function getHumanChoice() {
     let humanPlay;
     humanPlay = prompt("Whats's your play?", "Rock, paper or scissors");
     return humanPlay;
 }
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'scissors' && computerChoice == 'paper') || (humanChoice == 'paper' && computerChoice == 'rock')) {
+        console.log("Human wins.");
+        humanScore++;
+    } else if (((computerChoice == 'rock' && humanChoice == 'scissors') || (computerChoice == 'scissors' && humanChoice == 'paper') || (computerChoice == 'paper' && humanChoice == 'rock'))){
+        console.log("Computer wins.");
+        computerScore++;
+    } else if (humanChoice == computerChoice){
+        console.log("It's a draw.");
+    }
+    return humanScore, computerScore;
+}
+function playGame (){
+    for (let i = 0; i <= 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        humanScore, computerscore = playRound(humanChoice,computerChoice);
+    }
+    humanScore > computerScore ? console.log("HUMAN WINS") : console.log("COMPUTER WINS");
+}
 
 
 
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+playGame();
